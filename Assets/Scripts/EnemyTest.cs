be48515bac6bb81ed8766118cc5aa3974a58ece0
a23andru <143612630+a23andru@users.noreply.github.com>
2026-01-27 17:14:05 +0100

    Test Map, Lighting, Fog, Navmesh, Enemy
    Added a scene with limited light and a navmesh.
    Also a simple enemy that chases the player if they get too close

---
+++ Assets/Scripts/EnemyTest.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyTest : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    
+    private NavMeshPath path;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Aggro(Vector3 target)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            agent.destination = other.transform.position;
+        }
+    }
+}
