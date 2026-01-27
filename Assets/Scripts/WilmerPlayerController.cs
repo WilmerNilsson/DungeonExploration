89de0a9c137067fb3460fc869652f48314c958b5
WilmerNilsson <a23wilni@student.his.se>
2026-01-27 14:46:12 +0100

    Movement
    Fungerar med både tangentbord och mus, och controller

--- Assets/Scripts/WilmerPlayerController.cs
+++ Assets/Scripts/WilmerPlayerController.cs
@@ -1,26 +1,52 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class WilmerPlayerController : MonoBehaviour
 {
-    public Vector3 moveVector;
+    public Transform cameraTransform;
+    private Vector3 moveVector;
+    private Vector2 lookVector;
+    private Vector2 currentRotation;
+    public float moveSpeed;
+    public float sprintSpeed;
+    public float crouchSpeed;
+    private float currentSpeed;
+    public float jumpForce = 5;
+    public float mouseSensitivity;
+    public float stickSensitivity;
+
+    private Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    private void Awake()
+    {
+        rb =  GetComponent<Rigidbody>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += moveVector * Time.deltaTime;
+        currentRotation.y += lookVector.x;
+        currentRotation.x -= lookVector.y;
+        
+        currentRotation.x = Mathf.Clamp(currentRotation.x, -70, 70);
+        
+        cameraTransform.eulerAngles = currentRotation;
+        //moveVector = Vector3.Project(moveVector, new Vector3(cameraTransform.forward.x, 0 , cameraTransform.forward.z));
+        Vector3 rotatedVector = Quaternion.AngleAxis(cameraTransform.eulerAngles.y, Vector3.up) * moveVector;
+        transform.position += rotatedVector * Time.deltaTime * currentSpeed;
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            moveVector = context.ReadValue<Vector2>();
+            moveVector = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
         }
 
         if (context.canceled)
@@ -29,8 +55,49 @@ public class WilmerPlayerController : MonoBehaviour
         }
     }
 
-    public void OnLook()
+    public void OnMouseLook(InputAction.CallbackContext context)
     {
-        
+        lookVector = context.ReadValue<Vector2>() * mouseSensitivity;
+    }
+    
+    public void OnStickLook(InputAction.CallbackContext context)
+    {
+        lookVector = context.ReadValue<Vector2>() * stickSensitivity;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rb.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+        }
+    }
+    
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            currentSpeed = sprintSpeed;
+        }
+
+        if (context.canceled)
+        {
+            currentSpeed = moveSpeed;
+        }
+    }
+    
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            currentSpeed = crouchSpeed;
+            transform.localScale = new Vector3(1, 0.5f, 1);
+        }
+
+        if (context.canceled)
+        {
+            currentSpeed = moveSpeed;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
