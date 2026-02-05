de8801f07a61f3ad405e8341ff48dba13595573b
WilmerNilsson <a23wilni@student.his.se>
2026-02-05 13:12:41 +0100

    Added ground layers to player stair raycast

--- Assets/Scripts/Characters/Humanoid/HumanoidMovement.cs
+++ Assets/Scripts/Characters/Humanoid/HumanoidMovement.cs
@@ -20,6 +20,7 @@ public class HumanoidMovement : MonoBehaviour
     [Header("Stair raycast positions")]
     [SerializeField] private Transform upperRaycast;
     [SerializeField] private Transform lowerRaycast;
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float raycastDistance;
     [SerializeField] private float raycastAngle;
     [SerializeField] private float stepSmooth;
@@ -89,26 +90,23 @@ public class HumanoidMovement : MonoBehaviour
         RaycastHit upperHit;
         Debug.DrawRay(lowerRaycast.transform.position, rotatedVector.normalized * raycastDistance, Color.red);
         Debug.DrawRay(upperRaycast.transform.position, rotatedVector.normalized * raycastDistance, Color.green);
-        if (Physics.Raycast(lowerRaycast.transform.position, rotatedVector.normalized, out lowerHit, raycastDistance))
+        if (Physics.Raycast(lowerRaycast.transform.position, rotatedVector.normalized, out lowerHit, raycastDistance, groundLayer))
         {
-            Debug.Log(1);
-            if (!Physics.Raycast(upperRaycast.transform.position, rotatedVector.normalized, out upperHit, raycastDistance))
+            if (!Physics.Raycast(upperRaycast.transform.position, rotatedVector.normalized, out upperHit, raycastDistance, groundLayer))
             {
                 rb.position -= new Vector3(0f, -stepSmooth, 0f);
             }
         }
-        else if (Physics.Raycast(lowerRaycast.transform.position, minusAngle, out lowerHit, raycastDistance))
+        else if (Physics.Raycast(lowerRaycast.transform.position, minusAngle, out lowerHit, raycastDistance, groundLayer))
         {
-            Debug.Log(2);
-            if (!Physics.Raycast(upperRaycast.transform.position, minusAngle, out upperHit, raycastDistance))
+            if (!Physics.Raycast(upperRaycast.transform.position, minusAngle, out upperHit, raycastDistance, groundLayer))
             {
                 rb.position -= new Vector3(0f, -stepSmooth, 0f);
             }
         }
-        else if (Physics.Raycast(lowerRaycast.transform.position, plusAngle, out lowerHit, raycastDistance))
+        else if (Physics.Raycast(lowerRaycast.transform.position, plusAngle, out lowerHit, raycastDistance, groundLayer))
         {
-            Debug.Log(3);
-            if (!Physics.Raycast(upperRaycast.transform.position, plusAngle, out upperHit, raycastDistance))
+            if (!Physics.Raycast(upperRaycast.transform.position, plusAngle, out upperHit, raycastDistance, groundLayer))
             {
                 rb.position -= new Vector3(0f, -stepSmooth, 0f);
             }
