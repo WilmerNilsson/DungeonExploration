bc9e982e748bcfb4bfe5bd141152d4f1532f16d8
a23andru <143612630+a23andru@users.noreply.github.com>
2026-02-11 12:00:14 +0100

    got position working

--- Assets/Scripts/Characters/Humanoid/Mad_Adventurer/CrazedIK.cs
+++ Assets/Scripts/Characters/Humanoid/Mad_Adventurer/CrazedIK.cs
@@ -23,7 +23,12 @@ public class CrazedIK : MonoBehaviour
     
     [Header("Keyframe animation")]
     [SerializeField, Tooltip("List of keyframes for the animation to run through")] private CustomKeyframe[] Keyframes;
-    private int currentNode = 0;
+    [SerializeField] private CustomKeyframe targetKeyframe;
+    private CustomKeyframe currentKeyframe;
+    private int currentKeyIndex = 0;
+    
+    private bool lerpWeight = false;
+    private float startTime = 0;
     
     //a callback for calculating IK
     void OnAnimatorIK(int layerIndex)
@@ -35,73 +40,73 @@ public class CrazedIK : MonoBehaviour
                     animator.SetLookAtWeight(1);
                     animator.SetLookAtPosition(lookObj.position);
                 }
-
-                if (KeyframeAnimation)
+                if (currentKeyframe == null) //Lerp the weight to start
                 {
-                    currentPos = Vector3.Slerp(currentPos, transform.TransformDirection(Keyframes[currentNode].Position) + transform.position , lerpSpeed);
-                    currentRot = Quaternion.Slerp(currentRot, Keyframes[currentNode].Rotation, lerpSpeed);
-                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand,1);
-                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,1);  
-                    animator.SetIKPosition(AvatarIKGoal.RightHand,currentPos);
-                    animator.SetIKRotation(AvatarIKGoal.RightHand,currentRot);
+                    targetKeyframe = Keyframes[0];
+                    startTime = Time.time;
+                }
 
-                    if (Vector3.Distance(currentPos, Keyframes[currentNode].Position) < 0.1f)
-                    {
-                        currentNode = currentNode + 1;
-                        if (currentNode >= Keyframes.Length)
-                        {
-                            currentNode = 0;
-                            animator.SetBool("Attack",false);
-                        }
-                    }
+                lerpWeight = (currentKeyIndex == 0 || currentKeyIndex == targets.Length - 1);
+
+                if (lerpWeight)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand,Mathf.SmoothStep(0, 1, (Time.time - startTime) / targetKeyframe.Duration));
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,Mathf.SmoothStep(0, 1, (Time.time - startTime) / targetKeyframe.Duration));
+                    animator.SetIKPosition(AvatarIKGoal.RightHand,RelativePosition(targetKeyframe.Position));
+                    animator.SetIKRotation(AvatarIKGoal.RightHand,targetKeyframe.Rotation);
                 }
                 else
                 {
-                    if (target == null)
+                    currentPos = Vector3.Slerp(currentKeyframe.Position, RelativePosition(targetKeyframe.Position), (Time.time - startTime) / targetKeyframe.Duration);
+                    currentRot = Quaternion.Slerp(currentKeyframe.Rotation, targetKeyframe.Rotation, (Time.time - startTime) / targetKeyframe.Duration);
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand,1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,1);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand,currentPos);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand,currentRot);
+                }
+
+                if (Vector3.Distance(currentPos, targetKeyframe.Position) < 0.1f)
+                {
+                    currentKeyIndex++;
+                    if (currentKeyIndex >= Keyframes.Length)
                     {
-                        targetIndex = 0;
-                        target = targets[0];
+                        currentKeyIndex = 0;
+                        animator.SetBool("Attack",false);
+                        Reset();
                     }
-                    
-                    if(target != null) {
-                        
-                        currentPos = Vector3.Slerp(currentPos, target.position, lerpSpeed);
-                        currentRot = Quaternion.Slerp(currentRot, target.rotation, lerpSpeed);
-                        
-                        animator.SetIKPositionWeight(AvatarIKGoal.RightHand,1);
-                        animator.SetIKRotationWeight(AvatarIKGoal.RightHand,1);  
-                        animator.SetIKPosition(AvatarIKGoal.RightHand,currentPos);
-                        animator.SetIKRotation(AvatarIKGoal.RightHand,currentRot);
-                        
-                        if (Vector3.Distance(currentPos, target.position) < 0.1f)
-                        {
-                            targetIndex++;
-                            if (targetIndex >= targets.Length)
-                            {
-                                targetIndex = 0;
-                                target = null;
-                                animator.SetBool("Attack",false);
-                            }
-                            else
-                            {
-                                target = targets[targetIndex];
-                            }
-                        }
+                    else
+                    {
+                        currentKeyframe = Keyframes[currentKeyIndex-1];
+                        targetKeyframe = Keyframes[currentKeyIndex];
                     }
                 }
-                
             }
 
             //if the IK is not active, set the position and rotation of the hand and head back to the original position
             else {          
-                currentNode = 0;
-                targetIndex = 0;
-                // animator.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
-                // animator.SetIKRotationWeight(AvatarIKGoal.RightHand,0);
-                // animator.SetLookAtWeight(0);
-                // animationWeight = 0;
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand,0);
+                animator.SetLookAtWeight(0);
+                Reset();
             }
         }
+    }
+
+    private Vector3 RelativePosition(Vector3 position)
+    {
+        return transform.TransformDirection(position) + animator.rootPosition;
+    }
+
+    private Quaternion RelativeRotation(Quaternion rotation)
+    {
+        return rotation * animator.rootRotation;
+    }
+
+    private void Reset()
+    {
+        targetKeyframe = null;
+        currentKeyframe = null;
+        currentKeyIndex = 0;
     }
 }
 
@@ -110,7 +115,9 @@ class CustomKeyframe
 {
     [SerializeField] private Vector3 position;
     [SerializeField] private Vector3 rotation;
+    [SerializeField] private float duration;
     
     public Vector3 Position { get { return position; } }
     public Quaternion Rotation { get { return Quaternion.Euler(rotation); } }
+    public float Duration { get { return duration; } }
 }
