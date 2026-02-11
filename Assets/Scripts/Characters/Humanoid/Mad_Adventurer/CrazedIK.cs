6e5eeda0d92b826cba9f898efe9adbd0245cd517
a23andru <143612630+a23andru@users.noreply.github.com>
2026-02-11 15:32:56 +0100

    Frog swing works!
    gonna see if I can make it and arc with start and stop points instead

--- Assets/Scripts/Characters/Humanoid/Mad_Adventurer/CrazedIK.cs
+++ Assets/Scripts/Characters/Humanoid/Mad_Adventurer/CrazedIK.cs
@@ -8,66 +8,93 @@ public class CrazedIK : MonoBehaviour
     public Animator animator;
     [SerializeField, Tooltip("Where the avatar should look")] private Transform lookObj = null;
     
-    [Header("Hand transform targets")]
-    [SerializeField] private Vector3 currentPos;
-    [SerializeField] private Quaternion currentRot;
-    
     [Header("Keyframe animation")]
+    [SerializeField] private AttackState currentState = AttackState.Start;
     [SerializeField, Tooltip("List of keyframes for the animation to run through")] private CustomKeyframe[] Keyframes;
     [SerializeField] private CustomKeyframe targetKeyframe;
     private CustomKeyframe currentKeyframe;
-    private int currentKeyIndex = 0;
+    [SerializeField] private int currentKeyIndex = 0;
     
-    private bool lerpWeight = false;
     private float startTime = 0;
+    private float time = 0;
+    
+    
+
+    private enum AttackState
+    {
+        Start,
+        Swing,
+        Return
+    }
     
     //a callback for calculating IK
     void OnAnimatorIK(int layerIndex)
     {
         if(animator) {
             
-            if(attack) {
+            if(animator.GetBool("Attack")) {
                 if(lookObj != null) {
                     animator.SetLookAtWeight(1);
                     animator.SetLookAtPosition(lookObj.position);
                 }
+                
                 if (currentKeyframe == null) targetKeyframe = Keyframes[0];
                 if (startTime == 0) startTime = Time.time;
+                
+                time = (Time.time - startTime) / targetKeyframe.Duration;
 
-                lerpWeight = (currentKeyIndex == 0 || currentKeyIndex == Keyframes.Length - 1);
-
-                if (lerpWeight)
+                if (currentState == AttackState.Start)
                 {
-                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand,Mathf.SmoothStep(0, 1, (Time.time - startTime) / targetKeyframe.Duration));
-                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,Mathf.Lerp(0, 1, (Time.time - startTime) / targetKeyframe.Duration));
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand,Mathf.SmoothStep(0, 1, time));
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,Mathf.Lerp(0, 1, time));
                     animator.SetIKPosition(AvatarIKGoal.RightHand,RelativePosition(targetKeyframe.Position));
                     animator.SetIKRotation(AvatarIKGoal.RightHand,RelativeRotation(targetKeyframe.Rotation));
                 }
-                else
+                else if (currentState == AttackState.Swing)
                 {
-                    currentPos = Vector3.Slerp(currentKeyframe.Position, RelativePosition(targetKeyframe.Position), (Time.time - startTime) / targetKeyframe.Duration);
-                    currentRot = Quaternion.Slerp(currentKeyframe.Rotation, RelativeRotation(targetKeyframe.Rotation), (Time.time - startTime) / targetKeyframe.Duration);
                     animator.SetIKPositionWeight(AvatarIKGoal.RightHand,1);
                     animator.SetIKRotationWeight(AvatarIKGoal.RightHand,1);
-                    animator.SetIKPosition(AvatarIKGoal.RightHand,currentPos);
-                    animator.SetIKRotation(AvatarIKGoal.RightHand,currentRot);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand,RelativePosition(Vector3.Slerp(currentKeyframe.Position, targetKeyframe.Position, time)));
+                    animator.SetIKRotation(AvatarIKGoal.RightHand,RelativeRotation(Quaternion.Slerp(currentKeyframe.Rotation, targetKeyframe.Rotation, time)));
+                }
+                else if (currentState == AttackState.Return)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand,Mathf.SmoothStep(1, 0, time));
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,Mathf.Lerp(1, 0, time));
+                    animator.SetIKPosition(AvatarIKGoal.RightHand,RelativePosition(currentKeyframe.Position));
+                    animator.SetIKRotation(AvatarIKGoal.RightHand,RelativeRotation(currentKeyframe.Rotation));
                 }
 
-                if (((Time.time - startTime) / targetKeyframe.Duration) > 1f) //if its near the goal switch goal or end the attack
+                if (time > 1f) //if its near the goal switch goal or end the attack
                 {
-                    currentKeyIndex++;
-                    startTime = Time.time;
-                    if (currentKeyIndex > Keyframes.Length)
+                    if (currentKeyIndex == 0) // Start Swing
                     {
+                        currentKeyframe = Keyframes[currentKeyIndex];
+                        targetKeyframe = Keyframes[currentKeyIndex + 1];
+                        currentState = AttackState.Swing;
+                        currentKeyIndex++;
+                    }
+                    else if (currentKeyIndex < Keyframes.Length - 1) // Swing
+                    {
+                        currentKeyframe = Keyframes[currentKeyIndex];
+                        targetKeyframe = Keyframes[currentKeyIndex + 1];
+                        currentKeyIndex++;
+                    }
+                    else if (currentKeyIndex == Keyframes.Length - 1) // return
+                    {
+                        currentKeyframe = Keyframes[currentKeyIndex];
+                        currentState = AttackState.Return;
+                        currentKeyIndex++;
+                    }
+                    else // stop
+                    {
+                        Debug.Log("Keyframe animation finished");
                         currentKeyIndex = 0;
                         animator.SetBool("Attack",false);
+                        attack = false;
                         Reset();
                     }
-                    else
-                    {
-                        currentKeyframe = Keyframes[currentKeyIndex-1];
-                        targetKeyframe = Keyframes[currentKeyIndex];
-                    }
+                    startTime = Time.time;
                 }
             }
 
@@ -88,15 +115,19 @@ public class CrazedIK : MonoBehaviour
 
     private Quaternion RelativeRotation(Quaternion rotation)
     {
-        return rotation * animator.rootRotation;
+        Vector3 euler = rotation.eulerAngles;
+        euler.y += transform.parent.eulerAngles.y;
+        return Quaternion.Euler(euler);
     }
 
     private void Reset()
     {
+        currentState = AttackState.Start;
         targetKeyframe = null;
         currentKeyframe = null;
         currentKeyIndex = 0;
         startTime = 0;
+        time = 0;
     }
 }
 
