d4be40d1b0f55fbeb80c056e051f9ef3446bf80d
WilmerNilsson <a23wilni@student.his.se>
2026-02-12 12:04:31 +0100

    Hunger scriptable object

--- Assets/Scripts/Hunger.cs
+++ Assets/Scripts/Hunger.cs
@@ -4,39 +4,39 @@ using UnityEngine;
 
 public class Hunger : MonoBehaviour
 {
-    [Tooltip("The maximum amount of hunger")]
-    [SerializeField] private int maxHunger = 100;
-    public int currentHunger = 0;
-    [Tooltip("The health component of the player")]
+    [SerializeField] private PlayerHungerSO playerHungerSO;
     [SerializeField] private Health health;
-    private int hungerDamage = 0;
     
     [Tooltip("The amount of time between hunger ticks in seconds")]
     [SerializeField] private float hungerCooldown = 10;
-    
-    public static Hunger instance;
 
+    public static Hunger instance;
+    
     private IEnumerator HungerCoroutine()
     {
         yield return new WaitForSeconds(hungerCooldown);
         LoseHunger(1);
     }
 
+    public void ResetHunger()
+    {
+        playerHungerSO.currentHunger = playerHungerSO.maxHunger;
+    }
+
     private void Awake()
     {
         instance = this;
-        currentHunger = maxHunger;
         StartCoroutine(HungerCoroutine());
     }
 
     public void LoseHunger(int amount)
     {
-        currentHunger -= amount;
-        if (currentHunger < 0)
+        playerHungerSO.currentHunger -= amount;
+        if (playerHungerSO.currentHunger < 0)
         {
-            currentHunger = 0;
-            hungerDamage++;
-            health.TakeDamage(hungerDamage);
+            playerHungerSO.currentHunger = 0;
+            playerHungerSO.hungerDamage++;
+            health.TakeDamage(playerHungerSO.hungerDamage);
         }
 
         StartCoroutine(HungerCoroutine());
@@ -45,10 +45,11 @@ public class Hunger : MonoBehaviour
     public void Eat(int amount)
     {
         StopAllCoroutines();
-        currentHunger += amount;
-        if (currentHunger > maxHunger)
+        playerHungerSO.hungerDamage = 0;
+        playerHungerSO.currentHunger += amount;
+        if (playerHungerSO.currentHunger > playerHungerSO.maxHunger)
         {
-            currentHunger = maxHunger;
+            playerHungerSO.currentHunger = playerHungerSO.maxHunger;
         }
 
         StartCoroutine(HungerCoroutine());
---
+++ Assets/Scripts/PlayerHungerSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerHungerSO", menuName = "Scriptable Objects/PlayerHungerSO")]
+public class PlayerHungerSO : ScriptableObject
+{
+    [Tooltip("The maximum amount of hunger")]
+    [SerializeField] public int maxHunger = 100;
+    public int currentHunger = 0;
+    [Tooltip("The health component of the player")]
+    [SerializeField] private Health health;
+    public int hungerDamage = 0;
+}
