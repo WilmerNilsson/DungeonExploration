a66c69c8866b7a1ef4c9e7a897275ee2b80907b3
WilmerNilsson <a23wilni@student.his.se>
2026-02-17 15:29:01 +0100

    Minimap fixes

--- Assets/Scripts/Minimap/MinimapPart.cs
+++ Assets/Scripts/Minimap/MinimapPart.cs
@@ -16,6 +16,7 @@ public class MinimapPart : MonoBehaviour
 
     public Vector3 GetRotatedBounds()
     {
+        Debug.Log("rotate");
         float Y = transform.eulerAngles.y;
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z);
         Vector3 bounds = Renderer.bounds.extents * 2;
