b89702588bf549d89ac62310ca7ece47e222b642
TheGha <jonatanrehn@hotmail.com>
2026-02-27 11:52:20 +0100

    Fixed build errors

--- Assets/Scripts/UI/Inventory/ContainerInventory/InventoryGrid.cs
+++ Assets/Scripts/UI/Inventory/ContainerInventory/InventoryGrid.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
-using static Codice.Client.Commands.WkTree.WorkspaceTreeNode;
 
 [RequireComponent(typeof(RectTransform))]
 public class InventoryGrid : MonoBehaviour
