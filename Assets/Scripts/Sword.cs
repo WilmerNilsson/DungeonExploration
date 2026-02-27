531b4dd3aa2dbb895261b5cf7b879fd4f7f3ee42
a23andru <143612630+a23andru@users.noreply.github.com>
2026-02-27 17:24:51 +0100

    works even a bit more, figured out why the arc broke when rotated

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -14,14 +14,14 @@ public class Sword : Weapon
     [SerializeField, Tooltip("distance from middle toward start"), Range(0,1)] private float startBend;
     [SerializeField, Tooltip("distance from middle toward end"), Range(0,1)] private float endBend;
     [SerializeField, Tooltip("Attacks per Second")] private float attackSpeed;
-    [SerializeField] private float rightRotation;
+    [SerializeField] private float angle;
     [SerializeField] private bool x, y, z;
+    [SerializeField, Range(-360,360)] float xMod, yMod, zMod;
 
     private float time;
     private Vector3 swingStart;
     private Vector3 swingEnd;
     private Vector3 direction;
-    private float angle;
     
     private Vector3 heightPoint => swingStart + (swingEnd - swingStart) / 2 + headObj.forward * curveHeight;
     
@@ -30,10 +30,10 @@ public class Sword : Weapon
     private Transform arm => swordArm.data.mid;
     private Vector3 ShoulderToHand => hand.position - shoulder.position;
 
-    private Vector3 P0 => Quaternion.AngleAxis(angle, headObj.forward) * headObj.right;
-    private Vector3 P1 => Vector3.Lerp(P0,P3,startBend)+ headObj.forward * curveHeight;
-    private Vector3 P2 => Vector3.Lerp(P0,P3,endBend)+ headObj.forward * curveHeight;
-    private Vector3 P3 => Quaternion.AngleAxis(angle, headObj.forward) * -headObj.right + headObj.forward;
+    private Vector3 P0 => Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+    private Vector3 P1 => Vector3.Lerp(P0,P3,startBend)+ Vector3.forward * curveHeight;
+    private Vector3 P2 => Vector3.Lerp(P0,P3,endBend)+ Vector3.forward * curveHeight;
+    private Vector3 P3 => Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.left + Vector3.forward;
     // Update is called once per frame
     private void Start()
     {
@@ -52,16 +52,23 @@ public class Sword : Weapon
                 RandomiseStartEnd();
             }
             time += Time.deltaTime;
-            hand.position = shoulder.position + GetCurvePosition(time / attackSpeed);
+            Vector3 position = RotateVecAroundPoint(GetCurvePosition(time / attackSpeed), Quaternion.AngleAxis(core.transform.eulerAngles.y, Vector3.up), Vector3.zero );
+            hand.position = shoulder.position + position;
             
             Vector3 forward = GetCurveTangent(time / attackSpeed);
             Vector3 upward = GetCurveNormal(time / attackSpeed);
             
-            float xRot = x ? Vector3.Angle(core.forward, headObj.forward) - 90 : 0;
-            float yRot = y ? Vector3.Angle(GetCurvePosition(time / attackSpeed), P3) + 90: 0;
-            float zRot = z ? angle: 0;
-            Quaternion rotation = RelativeRotation(Quaternion.AngleAxis(zRot, core.forward) * Quaternion.AngleAxis(yRot, core.up) * Quaternion.AngleAxis(xRot, core.right));
-            hand.rotation = rotation;
+            
+            float xRot = x ? Vector3.Angle(core.forward, headObj.forward) + xMod: 0;
+            float yRot = y ? core.transform.eulerAngles.y + yMod: 0;
+            float zRot = z ? angle + zMod: 0;
+            //Quaternion rotation = Quaternion.AngleAxis(zRot, core.forward) * Quaternion.AngleAxis(yRot, headObj.up) * Quaternion.AngleAxis(xRot, core.right);
+            //hand.rotation = rotation;
+            
+            
+            //hand.up = position;
+            //hand.localRotation = Quaternion.LookRotation(Quaternion.AngleAxis(angle, core.forward) * Vector3.up,position);
+            hand.localRotation = Quaternion.Euler(xRot,yRot,zRot);
         }
         else
         {
@@ -73,7 +80,7 @@ public class Sword : Weapon
 
     private void RandomiseStartEnd()
     {
-        angle = Random.Range(-45, 45);
+        //angle = Random.Range(-45, 45);
     }
     
     private Quaternion RelativeRotation(Quaternion rotation)
@@ -97,9 +104,14 @@ public class Sword : Weapon
     
     private Vector3 GetCurveNormal(float t) // Doesnt really work atm
     {
-        Vector3 cross = Vector3.Cross(GetCurveTangent(t+0.0001f), GetCurveTangent(t));
+        Vector3 cross = Vector3.Cross(GetCurveTangent(t), GetCurvePosition(t));
         Vector3 normal = Vector3.Cross(cross, GetCurveTangent(t));
         return normal.normalized;
+    }
+    
+    private Vector3 RotateVecAroundPoint(Vector3 vector, Quaternion rotation, Vector3 point)
+    {
+        return rotation * (vector - point) + point;
     }
 
 
@@ -108,12 +120,12 @@ public class Sword : Weapon
         Gizmos.color = Color.black;
         Gizmos.DrawSphere(Vector3.zero, 0.1f);
         Gizmos.color = Color.green;
-        Gizmos.DrawSphere(shoulder.position + P0, 0.1f);
+        Gizmos.DrawSphere(P0, 0.1f);
         Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(shoulder.position + P1, 0.1f);
-        Gizmos.DrawSphere(shoulder.position + P2, 0.1f);
+        Gizmos.DrawSphere(P1, 0.1f);
+        Gizmos.DrawSphere(P2, 0.1f);
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(shoulder.position + P3, 0.1f);
+        Gizmos.DrawSphere(P3, 0.1f);
         
         
         Gizmos.color = Color.red;
