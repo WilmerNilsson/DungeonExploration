8dd4b0a380ed8eae192ca575f8aed1cc356b70cb
WilmerNilsson <a23wilni@student.his.se>
2026-03-02 15:05:38 +0100

    Town UI greeting

--- Assets/Scripts/Dialogue/DialogueTree.cs
+++ Assets/Scripts/Dialogue/DialogueTree.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DialogueTree", menuName = "Scriptable Objects/DialogueTree")]
 public class DialogueTree : ScriptableObject
 {
+    public int friendshipLevel = 1;
     public List<DialogueNode> Dialogues = new List<DialogueNode>();
+    public List<DialogueNode> Greetings  = new List<DialogueNode>();
+    public List<DialogueNode> Buy = new List<DialogueNode>();
+    public List<DialogueNode> Sell = new List<DialogueNode>();
+    public List<DialogueNode> NeutralMoney = new List<DialogueNode>();
 
     public void resetRead()
     {
         foreach (DialogueNode node in Dialogues)
         {
             node.HasBeenRead = false;
+        }
+    }
+
+    public void SetReadTrue(string name)
+    {
+        foreach (DialogueNode node in Dialogues)
+        {
+            if (node.Name == name)
+            {
+                node.HasBeenRead = true;
+                return;
+            }
         }
     }
 }
