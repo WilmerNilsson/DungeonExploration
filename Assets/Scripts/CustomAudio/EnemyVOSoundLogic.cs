4b6fb11caa220ea81c0803db8fc4834ebb9f30a1
coralsleeper <carl.erland.gabriel@gmail.com>
2026-03-05 16:09:36 +0100

    combat musik saker i enemySoundLogic istället för enemy states + swing sound tillbaka

--- Assets/Scripts/CustomAudio/EnemyVOSoundLogic.cs
+++ Assets/Scripts/CustomAudio/EnemyVOSoundLogic.cs
@@ -41,15 +41,17 @@ public class EnemyVOSoundLogic : MonoBehaviour
     
     private MadAventurerBaseState _lastState = new MadAdventurerIdleState();
     
-    public void OnMadStateChange(MadAventurerBaseState newState) //TODO: fixa så att den använder nya systemet när det finns
+    public void OnMadStateChange(MadAventurerBaseState newState)
     {
         if (newState.GetType() == typeof(MadAdventurerChasingState) && _lastState.GetType() == typeof(MadAdventurerIdleState))
         {
             AudioManager.Instance.SetParameter(enemyVoPath, parameters.state, 1, gameObject);
+            CombatChecker.AddToChaseList(gameObject);
         }
         if (_lastState.GetType() == typeof(MadAdventurerChasingState) && newState.GetType() == typeof(MadAdventurerIdleState))
         {
             AudioManager.Instance.SetParameter(enemyVoPath, parameters.state, 0, gameObject);
+            CombatChecker.RemoveFromChaseList(gameObject);
         }
         _lastState = newState;
     }
@@ -88,6 +90,7 @@ public class EnemyVOSoundLogic : MonoBehaviour
         AudioManager.Instance.SetParameter(enemyVoPath, parameters.death, 1, gameObject);
         AudioManager.Instance.StopEvent(enemyVoPath, STOP_MODE.ALLOWFADEOUT, gameObject);
         AudioManager.Instance.ReleaseInstance(enemyVoPath, gameObject);
+        CombatChecker.RemoveFromChaseList(gameObject);
     }
     
     private void OnDestroy()
@@ -95,5 +98,6 @@ public class EnemyVOSoundLogic : MonoBehaviour
         if (!AudioManager.IsValid) return;
         AudioManager.Instance.StopEvent(enemyVoPath, STOP_MODE.ALLOWFADEOUT, gameObject);
         AudioManager.Instance.ReleaseInstance(enemyVoPath, gameObject);
+        CombatChecker.RemoveFromChaseList(gameObject);
     }
 }
