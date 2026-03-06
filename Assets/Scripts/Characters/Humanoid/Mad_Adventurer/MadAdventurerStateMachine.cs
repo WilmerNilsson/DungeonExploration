1c388707898f8595e8c05f82da5efbb49d30d374
coralsleeper <carl.erland.gabriel@gmail.com>
2026-03-06 10:09:53 +0100

    Revert "Squashed commit of the following:"
    This reverts commit eafbcfdf3a8dd55a6c34f70387d676a2ff90a817.

--- Assets/Scripts/Characters/Humanoid/Mad_Adventurer/MadAdventurerStateMachine.cs
+++ Assets/Scripts/Characters/Humanoid/Mad_Adventurer/MadAdventurerStateMachine.cs
@@ -109,6 +109,12 @@ public class MadAdventurerStateMachine : MonoBehaviour
         Controller.Attack(angle);
     }
 
+    public void Attack()
+    {
+        float angle = Random.Range(-160f, 160f);
+        Controller.Attack(angle);
+    }
+
     public void Die()
     {
         Transit(DyingState);
