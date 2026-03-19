a4a218c75a92c4234ab8b95a344ca1335573cdfa
TheGha <jonatanrehn@hotmail.com>
2026-03-19 11:02:41 +0100

    Breakable planks save

--- Assets/Scripts/WorldObjects/BreakablePlank.cs
+++ Assets/Scripts/WorldObjects/BreakablePlank.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BreakablePlank : MonoBehaviour,  IEnabledHelper
 {
-    private bool broken;
+    private bool broken = false;
 
     public void Break()
     {
@@ -11,12 +11,16 @@ public class BreakablePlank : MonoBehaviour,  IEnabledHelper
 
     public void EnableFromSave()
     {
+        Debug.Log("aaaaaaaaa");
+
         broken = true;
         gameObject.SetActive(false);
     }
 
     public bool IsEnabledForSave()
     {
+        Debug.Log("bbbbbbbb: " + broken);
+
         return broken;
     }
 }
