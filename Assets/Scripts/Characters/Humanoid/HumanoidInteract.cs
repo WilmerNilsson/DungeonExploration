50660e0d4196fe9b5df01ed1be89dd5598fbe0ea
TheGha <jonatanrehn@hotmail.com>
2026-03-24 13:12:28 +0100

    Interact and dead, equipment slots item fix
    Humanoid interact no longer works when dead.
    Humanoid interact no longer does OnUnSee every frame when a object is not seen.
    
    Equipment slots only take UIWeapon, controlled by a bool on ItemGrid.

--- Assets/Scripts/Characters/Humanoid/HumanoidInteract.cs
+++ Assets/Scripts/Characters/Humanoid/HumanoidInteract.cs
@@ -7,75 +7,87 @@ public class HumanoidInteract : MonoBehaviour
     [SerializeField] protected bool debug = false;
     [SerializeField] private Transform head;
     [SerializeField] private float interactDistance = 5f;
+    [SerializeField] private Health health;
     private Interactable interactable;
-    
+
     [Tooltip("The layers that will block the ray, including the interactable object")]
     [SerializeField] private LayerMask layerMask;
     private RaycastHit hit;
 
     public UnityEvent OnSee, OnUnSee;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+#if UNITY_EDITOR
+    private void OnValidate()
     {
-        
+        if (health == null) Debug.LogWarning("health is null", this);
+        if (head == null) Debug.LogWarning("head transform is null", this);
     }
+#endif
+
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(head.position, head.forward, out hit, interactDistance, layerMask))
+        if (!health.Dead && Physics.Raycast(head.position, head.forward, out hit, interactDistance, layerMask))
         {
             if (hit.transform.gameObject.TryGetComponent(out Interactable newInteractable))
             {
-                if(debug)Debug.DrawRay(head.position, head.forward * hit.distance, Color.green);
-                //Debug.Log("Did Hit");
-                
+                if (debug)
+                {
+                    //Debug.DrawRay(head.position, head.forward * hit.distance, Color.green);
+                    Debug.Log("Did Hit");
+                }
+
                 if (interactable != newInteractable)
                 {
                     if (interactable)
                     {
                         interactable.OnView(false);
                     }
+
                     interactable = newInteractable;
                     interactable.OnView(true);
-                    if (interactable.canInteract) OnSee?.Invoke();
+                    if (interactable.canInteract)
+                    {
+                        OnSee?.Invoke();
+                    }
                 }
                 else if (!interactable.canInteract)
                 {
-                    OnUnSee.Invoke();
+                    OnUnSee?.Invoke();
                 }
-                //UIText.SetActive(true);
             }
             else
             {
-                if(debug)Debug.DrawRay(head.position, head.forward * hit.distance, Color.yellow);
-                //Debug.Log("Hit something else");
-                if (interactable)
+                if (debug)
                 {
-                    interactable.OnView(false);
-                    OnUnSee?.Invoke();
+                    //Debug.DrawRay(head.position, head.forward * hit.distance, Color.yellow);
+                    Debug.Log("Hit something else: " + (interactable != null).ToString());
                 }
-                interactable = null;
-                //UIText.SetActive(false);
+
+                if (((object) interactable) != null)
+                {
+                    if (interactable) interactable.OnView(false);
+                    OnUnSee?.Invoke();
+                    interactable = null;
+                }
             }
         }
         else
         {
-            if(debug)Debug.DrawRay(head.position, head.forward * interactDistance, Color.red);
-            //Debug.Log("Did not Hit");
-            if (interactable)
+            if(debug)
             {
-                interactable.OnView(false);
-                OnUnSee?.Invoke();
+                //Debug.DrawRay(head.position, head.forward * interactDistance, Color.red);
+                Debug.Log("Did not Hit: " + (interactable != null).ToString());
             }
-            interactable = null;
-            //UIText.SetActive(false);
-        }
 
-        if (!interactable)
-        {
-            OnUnSee?.Invoke();
+            if (((object) interactable) != null)
+            {
+                if(interactable) interactable.OnView(false);
+                OnUnSee?.Invoke();
+                interactable = null;
+            }
         }
     }
 
